805781747a278a3fc54dc4322f16df10ce5e95a9
herok <heroking2081@gmail.com>
2022-11-25 17:35:28 +0500

    0.2. Внутри ArrayList [#504814]

---
+++ src/main/java/ru/job4j/list/ListUsage.java
@@ -0,0 +1,20 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListUsage {
+    public static void main(String[] args) {
+        List<String> rsl = new ArrayList<>();
+        rsl.add("one");
+        rsl.add("two");
+        rsl.add("three");
+        List<String> list = new ArrayList<>();
+        list.add("four");
+        list.add("five");
+        rsl.addAll(2, list);
+        for (String s : rsl) {
+            System.out.println("Текущий элемент: " + s);
+        }
+    }
+}
