6fec3a0f68e296e0f18dbb6fb2c412985b48aa43
herok <heroking2081@gmail.com>
2022-12-11 15:47:23 +0500

    5.1. Именованные аргументы [#295518 #66628]

---
+++ src/test/java/ru/job4j/io/ArgsNameTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ArgsNameTest {
+
+    @Test
+    void whenGetFirst() {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        assertThat(jvm.get("Xmx")).isEqualTo("512");
+    }
+
+    @Test
+    void whenGetFirstReorder() {
+        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        assertThat(jvm.get("Xmx")).isEqualTo("512");
+    }
+
+    @Test
+    void whenWrongSomeArgument() {
+        assertThatThrownBy(() -> ArgsName.of(new String[]{}))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
